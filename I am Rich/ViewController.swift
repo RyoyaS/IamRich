9cf15693e05c185bf8a66a91fe213758d06cf426
RyoyaS <ryouya1998.1.21@gmail.com>
2021-12-08 21:44:25 +0900

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 鈴木涼也 on 2021/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
